bd7e82dfa22f1dd32ef9e8fd7baa5a61e3e055ac
Unai Sainz <unaihtc70@gmail.com>
2023-05-02 19:18:32 +0200

    add some skeleton for lightning modules

---
+++ td-compression/src/td-comp/lightning-modules/tn_model.py
@@ -0,0 +1,55 @@
+import torch
+import torch.nn as nn
+import lightning.pytorch as pl
+from torchmetrics.functional import accuracy
+
+class TNModel(pl.LightningModule):
+    def __init__(self, tn_model, init_lr=1e-3):
+        super().__init__()
+
+        self.model = tn_model
+        self.loss = nn.CrossEntropyLoss()
+        self.init_lr = init_lr
+
+    def forward(self, x):
+        # in lightning, forward defines the prediction/inference actions
+        logits = self.model(x)
+        return logits
+
+    def training_step(self, batch, batch_idx):
+        _, loss, acc = self._get_preds_loss_accuracy(batch)
+
+        # Log loss and metric
+        self.log('train_loss', loss)
+        self.log('train_accuracy', acc)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        preds, loss, acc = self._get_preds_loss_accuracy(batch)
+
+        # Log loss and metric
+        self.log('val_loss', loss)
+        self.log('val_accuracy', acc)
+        return preds
+    
+    def test_step(self, batch, batch_idx):
+        _, loss, acc = self._get_preds_loss_accuracy(batch)
+
+        # Log loss and metric
+        self.log('test_loss', loss)
+        self.log('test_accuracy', acc)
+
+    def _get_preds_loss_accuracy(self, batch):
+        """
+        convenience function since train/valid/test steps are similar
+        """
+        x, y = batch
+        logits = self(x)
+        preds = torch.argmax(logits, dim=1)
+        loss = self.loss(logits, y)
+        acc = accuracy(preds, y, 'multiclass', num_classes=10)
+        return preds, loss, acc
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.init_lr)
+        return optimizer
